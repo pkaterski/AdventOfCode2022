31605af500e2ff7c89a37827c9c26bd0f4a78d2f
Parashkev Katerski <pkaterski@gmail.com>
2022-12-01 23:50:35 +0200

    do O(n) insted of O(n * log(n))

--- day1/part2.js
+++ day1/part2.js
@@ -3,7 +3,9 @@ const log = console.log;
 
 const handleFile = (err, input) => {
   const totalCals = input.split('\n\n').map(i => i.split('\n').map(i => +i).reduce((acc, val) => acc + val))
-  const top3CalsTotal = totalCals.sort((a, b) => a - b).slice(-3).reduce((acc, val) => acc + val)
+  const top3CalsTotal = totalCals
+    .reduce((acc, val) => [...acc, val].sort((a, b)=>(a - b)).slice(-3), [])
+    .reduce((acc, val) => acc + val)
   log(top3CalsTotal)
 }
 
