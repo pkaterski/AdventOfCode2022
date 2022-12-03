41d630fd37be164d3dc110a92982c212cec5e796
Parashkev Katerski <pkaterski@gmail.com>
2022-12-03 13:31:35 +0200

    simplify partitioning

--- day3/part2.js
+++ day3/part2.js
@@ -5,22 +5,19 @@ const handleFile = (err, input) => {
   const lines = input
     .split('\n')
     .slice(0, -1)
-  const arr0s = lines.filter((_el, index) => index % 3 === 0)
-  const arr1s = lines.filter((_el, index) => index % 3 === 1)
-  const arr2s = lines.filter((_el, index) => index % 3 === 2)
 
-  const combined = arr0s.map((_el, index) => [arr0s[index], arr1s[index], arr2s[index]])
+  const partition3 = lines
+    .reduce((acc, val, index) => index % 3 ? [...acc.slice(0 ,-1), [...acc.slice(-1,)[0], val]] : [...acc, [val]], [])
 
-  const result = combined
+  const result = partition3
     .map(([a, b, c]) => {
       const common = [...a].filter(el => [...b].includes(el) && [...c].includes(el))
       const val = [...new Set(common)][0]
       const num = val.charCodeAt(0) - 96
-      const normalizedNum = num > 0 ? num : num + 31 + 27
-      return normalizedNum
+      return num > 0 ? num : num + 31 + 27
     })
     .reduce((acc, val) => acc + val)
-  console.log(result)
+  log(result)
 }
 
 fs.readFile('./input.txt', 'utf8', handleFile)
