e6e87e0b21c055bb0bf639de44b83690d0116b9b
Parashkev Katerski <pkaterski@gmail.com>
2022-12-04 09:05:34 +0200

    add day 4 solution

---
+++ day4/part1.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const log = console.log;
+
+const handleFile = (err, input) => {
+  const result = input
+    .split('\n')
+    .slice(0, -1)
+    .map(i => i.split(',').map(j => j.split('-').map(k => +k)))
+    .reduce((acc, [[a1,b1],[a2,b2]]) => a1 >= a2 && b1 <= b2 || a2 >= a1 && b2 <= b1 ? acc + 1 : acc, 0)
+  
+  log(result)
+}
+
+fs.readFile('./input.txt', 'utf8', handleFile)
+
